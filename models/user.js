15fd8400897332714a26463db10843165eebe9cb
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-03-17 15:58:43 +0900

    Hash user password before creating user

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 // {
 //     email: 'khwalex@gmail.com',
@@ -85,6 +86,21 @@ UserSchema.statics.findByToken = async function (token) {
         return Promise.reject();
     }
 }
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
 
 var User = mongoose.model('User', UserSchema);
 
